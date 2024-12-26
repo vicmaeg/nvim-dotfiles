c8808aa238278ddce6d185364762ecef2cc4467b
Victor Martos <vmartos@outlook.com>
2024-12-26 19:47:20 +0100

    feat: add roslyn plugin for LSP

---
+++ lua/plugins/roslyn.lua
@@ -0,0 +1,8 @@
+return {
+  -- Use roslyn custom plugin for .NET LSP
+  "seblj/roslyn.nvim",
+  ft = "cs",
+  opts = {
+    -- your configuration comes here; leave empty for default settings
+  },
+}
