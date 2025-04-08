a11371a88815837f0779d349c0fd262063cfd8f0
vicmaeg <vmartos@outlook.com>
2025-04-09 00:25:45 +0200

    chore: format theme.lua

--- lua/vimarege/plugins/theme.lua
+++ lua/vimarege/plugins/theme.lua
@@ -1,9 +1,7 @@
 return {
-	{
-		"rebelot/kanagawa.nvim",
-		priority = 1000,
-		config = function()
-			vim.cmd.colorscheme 'kanagawa'
-		end,
-	}
+  "rebelot/kanagawa.nvim",
+  priority = 1000,
+  config = function()
+    vim.cmd.colorscheme 'kanagawa'
+  end,
 }
