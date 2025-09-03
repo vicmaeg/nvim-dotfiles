d6e61e80b977aa16ff9d237f4331c4fc458a8d1e
vicmaeg <vmartos@outlook.com>
2025-09-03 23:29:10 +0200

    feat: add extra mason registries

--- lua/vicmaeg/plugins/lsp.lua
+++ lua/vicmaeg/plugins/lsp.lua
@@ -231,6 +231,13 @@ return {
       },
     }
 
+	require("mason").setup({
+	    registries = {
+		"github:mason-org/mason-registry",
+		"github:Crashdummyy/mason-registry",
+	    },
+	})
+
     require("mason-lspconfig").setup({
       ensure_installed = servers,
       handlers = {
