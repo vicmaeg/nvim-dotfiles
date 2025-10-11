66a1865968e453d1754f59f7e315dbb55bec91db
Victor Martos <v.martos@fourthline.com>
2025-10-11 15:22:22 +0200

    feat: bring back kanagawa theme and remove gruvbox

--- lua/vicmaeg/plugins/theme.lua
+++ lua/vicmaeg/plugins/theme.lua
@@ -3,14 +3,7 @@ return {
     "rebelot/kanagawa.nvim",
     priority = 1000,
     config = function()
-      -- vim.cmd.colorscheme("kanagawa")
-    end,
-  },
-  {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("gruvbox")
+      vim.cmd.colorscheme("kanagawa")
     end,
   },
 }
